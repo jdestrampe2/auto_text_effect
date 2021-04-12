8551d869f86ea42c7ee0916ab11b0a9a41d35d10
Joseph C. Destrampe <gaiagreenworks@gmail.com>
2021-04-12 14:22:43 -0700

    Finish CSS and JS

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-const textEl = document.getElementById('text')
-const speedEl = document.getElementById('speed')
-const text = 'We Love Programming!'
-let idx = 1
-let speed = 300 / speedEl.value
+const textEl = document.getElementById('text');
+const speedEl = document.getElementById('speed');
+const text = 'We Love Programming!';
 
-writeText()
+let idx = 1;
+let speed = 300 / speedEl.value;
+
+writeText();
 
 function writeText() {
-    textEl.innerText = text.slice(0, idx)
+	textEl.innerText = text.slice(0, idx);
+	idx++;
 
-    idx++
+	if (idx > text.length) {
+		idx = 1;
+	}
 
-    if(idx > text.length) {
-        idx = 1
-    }
-
-    setTimeout(writeText, speed)
+	setTimeout(writeText, speed);
 }
 
-
-speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
+speedEl.addEventListener('input', (e) => (speed = 300 / e.target.value));
